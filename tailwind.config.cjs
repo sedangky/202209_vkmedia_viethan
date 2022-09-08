28b61614a49b201d7c63599416ab25d6f16958b4
sedangky <sedangky@gmail.com>
2022-09-09 00:13:04 +0700

    product page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
-		require("daisyui")
+		require('daisyui'),
 	],
+	daisyui: {
+		styled: true,
+		themes: false,
+		base: true,
+		utils: true,
+		logs: false,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+	},
 }
